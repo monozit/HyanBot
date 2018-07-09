f1912cdfb870bfff5babff7f8726691066a61812
castaneai <castaneai@castaneai.net>
2018-07-09 22:42:50 +0900

    Update README and packages

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # HyanBot
 
 ひゃんbotです
+
+## 実行方法
+`twitter4j.properties.example` を `twitter4j.properties` にリネームし、Twitter API Key を埋める
+
+```sh
+./hyan.sh  # UNIX 系
+./hyan.bat  # Windows 系
+```
---
+++ hyan.bat
@@ -0,0 +1,3 @@
+javac -d build/ -cp "packages/twitter4j/*;packages/jackson/*;packages/apache_http_client/*" -encoding UTF-8 src/*.java
+java -cp "src/;packages/twitter4j/*;packages/jackson/*;packages/apache_http_client/*" build/HyanBot
+
